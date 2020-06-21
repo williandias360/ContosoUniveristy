bf71bbb55c366857b42078d0e04c76005a4ab63e
Willian Dias <willian.dias360@hotmail.com>
2020-06-21 14:47:50 -0300

    Scaffold Student

--- Models/Enrollment.cs
+++ Models/Enrollment.cs
@@ -2,7 +2,7 @@ namespace ContosoUniversity.Models
 {
     public class Enrollment
     {
-        public int EnrollmentIdD { get; set; }
+        public int EnrollmentID { get; set; }
         public int CourseID { get; set; }
         public int StudentID { get; set; }
         public Grade? Grade { get; set; }
